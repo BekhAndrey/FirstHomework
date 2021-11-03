300c217366bfca29775b4d1d9abd94e2bf607c10
BekhAndrey <andreyac17@gmail.com>
2021-11-03 17:11:25 +0300

    Mode selection added

--- FirstHomework-pt.2/src/com/bekh/Main.java
+++ FirstHomework-pt.2/src/com/bekh/Main.java
@@ -1,8 +1,32 @@
 package com.bekh;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        TicTacToe ticTacToe = new TicTacToe();
+        while(true){
+            System.out.println("Please, enter game mode");
+            System.out.println("1 - human vs human;");
+            System.out.println("2 - human vs AI;");
+            System.out.println("3 - exit");
+            int choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    ticTacToe.playVsHuman();
+                    break;
+                case 2:
+                    ticTacToe.playVsAI();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid input;");
+                    break;
+            }
+
+        }
     }
 }
